53f862f08024bda9a32b561e8c3d9e0c29b00bd0
Nico4899 <nico.fliegel@gmail.com>
2024-02-18 14:09:36 +0100

    changes

--- src/internal_events.c
+++ src/internal_events.c
@@ -1,40 +1,39 @@
+#include "gamepad.h"
 #include "internal_events.h"
 #include <stdlib.h>
 
-// Initialize callback function pointers and context pointers to NULL
-void (*Gamepad_deviceAttachCallback)(struct Gamepad_device *device, void *context) = NULL;
-void (*Gamepad_deviceRemoveCallback)(struct Gamepad_device *device, void *context) = NULL;
-void (*Gamepad_buttonDownCallback)(struct Gamepad_device *device, unsigned int buttonID, double timestamp, void *context) = NULL;
-void (*Gamepad_buttonUpCallback)(struct Gamepad_device *device, unsigned int buttonID, double timestamp, void *context) = NULL;
-void (*Gamepad_axisMoveCallback)(struct Gamepad_device *device, unsigned int axisID, float value, float lastValue, double timestamp, void *context) = NULL;
-void *Gamepad_deviceAttachContext = NULL;
-void *Gamepad_deviceRemoveContext = NULL;
-void *Gamepad_buttonDownContext = NULL;
-void *Gamepad_buttonUpContext = NULL;
-void *Gamepad_axisMoveContext = NULL;
+void (* Gamepad_deviceAttachCallback)(struct Gamepad_device * device, void * context) = NULL;
+void (* Gamepad_deviceRemoveCallback)(struct Gamepad_device * device, void * context) = NULL;
+void (* Gamepad_buttonDownCallback)(struct Gamepad_device * device, unsigned int buttonID, double timestamp, void * context) = NULL;
+void (* Gamepad_buttonUpCallback)(struct Gamepad_device * device, unsigned int buttonID, double timestamp, void * context) = NULL;
+void (* Gamepad_axisMoveCallback)(struct Gamepad_device * device, unsigned int buttonID, float value, float lastValue, double timestamp, void * context) = NULL;
+void * Gamepad_deviceAttachContext = NULL;
+void * Gamepad_deviceRemoveContext = NULL;
+void * Gamepad_buttonDownContext = NULL;
+void * Gamepad_buttonUpContext = NULL;
+void * Gamepad_axisMoveContext = NULL;
 
-// Registration functions for device attach/detach and button/axis events
-void Gamepad_deviceAttachFunc(void (*callback)(struct Gamepad_device *device, void *context), void *context) {
-    Gamepad_deviceAttachCallback = callback;
-    Gamepad_deviceAttachContext = context;
+void Gamepad_deviceAttachFunc(void (* callback)(struct Gamepad_device * device, void * context), void * context) {
+	Gamepad_deviceAttachCallback = callback;
+	Gamepad_deviceAttachContext = context;
 }
 
-void Gamepad_deviceRemoveFunc(void (*callback)(struct Gamepad_device *device, void *context), void *context) {
-    Gamepad_deviceRemoveCallback = callback;
-    Gamepad_deviceRemoveContext = context;
+void Gamepad_deviceRemoveFunc(void (* callback)(struct Gamepad_device * device, void * context), void * context) {
+	Gamepad_deviceRemoveCallback = callback;
+	Gamepad_deviceRemoveContext = context;
 }
 
-void Gamepad_buttonDownFunc(void (*callback)(struct Gamepad_device *device, unsigned int buttonID, double timestamp, void *context), void *context) {
-    Gamepad_buttonDownCallback = callback;
-    Gamepad_buttonDownContext = context;
+void Gamepad_buttonDownFunc(void (* callback)(struct Gamepad_device * device, unsigned int buttonID, double timestamp, void * context), void * context) {
+	Gamepad_buttonDownCallback = callback;
+	Gamepad_buttonDownContext = context;
 }
 
-void Gamepad_buttonUpFunc(void (*callback)(struct Gamepad_device *device, unsigned int buttonID, double timestamp, void *context), void *context) {
-    Gamepad_buttonUpCallback = callback;
-    Gamepad_buttonUpContext = context;
+void Gamepad_buttonUpFunc(void (* callback)(struct Gamepad_device * device, unsigned int buttonID, double timestamp, void * context), void * context) {
+	Gamepad_buttonUpCallback = callback;
+	Gamepad_buttonUpContext = context;
 }
 
-void Gamepad_axisMoveFunc(void (*callback)(struct Gamepad_device *device, unsigned int axisID, float value, float lastValue, double timestamp, void *context), void *context) {
-    Gamepad_axisMoveCallback = callback;
-    Gamepad_axisMoveContext = context;
+void Gamepad_axisMoveFunc(void (* callback)(struct Gamepad_device * device, unsigned int axisID, float value, float lastValue, double timestamp, void * context), void * context) {
+	Gamepad_axisMoveCallback = callback;
+	Gamepad_axisMoveContext = context;
 }
